072f3a5cb2e4a89c006b78a286a8faad694b7215
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2024-05-31 09:27:52 -0700

    Fixed header padding to prevent mobile wrap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex bg-gray-900 h-14 items-center justify-between sm:justify-start px-4 sm:px-8 gap-x-4 sm:gap-x-8">
+        <header className="flex bg-gray-900 h-14 items-center justify-between sm:justify-start px-3 sm:px-8 gap-x-2 sm:gap-x-8">
             <div>
-                <Link href="/" className="text-lg font-bold py-4 hover:text-purple-300 transition">Elliot Daniels</Link>
+                <Link href="/" className="sm:text-lg font-bold py-4 hover:text-purple-300 transition">Elliot Daniels</Link>
             </div>
-            <ul className="flex gap-x-4 items-center">
+            <ul className="flex gap-x-3 sm:gap-x-4 items-center">
                 <li>
                     <Link href="/" className="py-4 hover:text-purple-300 transition">Home</Link>
                 </li>
