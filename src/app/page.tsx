e3cb0a982b5bfaa8c734cc4d8eefa88d1c91d45b
Elliot Daniels <elliotdanielsseattle@gmail.com>
2024-12-11 11:33:40 -0800

    changed hobbies titles to robotics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/></svg>
             </div>
             <b className="group-hover:translate-x-1 transition">
-              Hobbies
+              Robotics
             </b>
           </Link>
         </motion.div>
